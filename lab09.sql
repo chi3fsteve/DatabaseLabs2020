b287ab28009571ae97e3bcbf9903ff79e478a253
chi3fsteve <48816706+chi3fsteve@users.noreply.github.com>
2021-09-29 15:41:09 +0200

    Update lab09.sql

--- lab09.sql
+++ lab09.sql
@@ -8,10 +8,29 @@ GO
 --2
 IF OBJECT_ID('FOREMEN','V') IS NOT NULL DROP VIEW FOREMEN
 GO
-CREATE VIEW FOREMEN ASSELECT ID, Name, Salary_base, Years_of_employment=DATEDIFF(year, Employed, GETDATE())FROM StaffGO--3IF OBJECT_ID('SALARIES','V') IS NOT NULL DROP VIEW SALARIES
+CREATE VIEW FOREMEN AS
+SELECT ID, Name, Salary_base, Years_of_employment=DATEDIFF(year, Employed, GETDATE())
+FROM Staff
+GO
+
+--3
+IF OBJECT_ID('SALARIES','V') IS NOT NULL DROP VIEW SALARIES
 GO
 CREATE VIEW SALARIES AS
-SELECT Branch, Average=AVG(Salary_base+ISNULL(Salary_extra, 0)), Salary_min=MIN(Salary_base+ISNULL(Salary_extra, 0)),		Salary_max=MAX(Salary_base+ISNULL(Salary_extra, 0)), Fund=SUM(Salary_base+ISNULL(Salary_extra, 0)),		Qty_salary_base=COUNT(Salary_base), Qty_salary_ext=COUNT(Salary_extra)FROM StaffGROUP BY BranchGOSELECT a.Name, a.Salary_base, b.AverageFROM Staff aINNER JOIN (SELECT Branch, Average FROM SALARIES) b ON a.Branch=b.BranchWHERE a.Salary_base<b.AverageGO--4IF OBJECT_ID('SALARIES_BASE','V') IS NOT NULL DROP VIEW SALARIES_BASE
+SELECT Branch, Average=AVG(Salary_base+ISNULL(Salary_extra, 0)), Salary_min=MIN(Salary_base+ISNULL(Salary_extra, 0)),
+		Salary_max=MAX(Salary_base+ISNULL(Salary_extra, 0)), Fund=SUM(Salary_base+ISNULL(Salary_extra, 0)),
+		Qty_salary_base=COUNT(Salary_base), Qty_salary_ext=COUNT(Salary_extra)
+FROM Staff
+GROUP BY Branch
+GO
+
+SELECT a.Name, a.Salary_base, b.Average
+FROM Staff a
+INNER JOIN (SELECT Branch, Average FROM SALARIES) b ON a.Branch=b.Branch
+WHERE a.Salary_base<b.Average
+GO
+--4
+IF OBJECT_ID('SALARIES_BASE','V') IS NOT NULL DROP VIEW SALARIES_BASE
 GO
 CREATE VIEW SALARIES_BASE AS
 SELECT Branch, Name, Salary_base
